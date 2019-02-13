bf8316d4e58c539dcecc749efcde3dfd8226997b
Sebastien Besson <seb.besson@gmail.com>
2019-02-13 22:09:08 +0000

    Update channel names & colors and physical sizes

--- scripts/generate_companions_experimentA.py
+++ scripts/generate_companions_experimentA.py
@@ -15,6 +15,8 @@ IMAGE_TYPES = {
     'conc': 'conctif',
 }
 
+PROTEINS = ["SMC4", "NCAPD3", "NCAPD2", "NCAPDH2", "NCAPH"]
+
 BASE_DIRECTORY = join(
     dirname(abspath(dirname(sys.argv[0]))), 'experimentA', 'companions')
 folders = [join(BASE_DIRECTORY, x) for x in os.listdir(BASE_DIRECTORY)]
@@ -33,11 +35,21 @@ for folder in folders:
             os.path.basename, glob.glob("%s/rawtif/*" % cell)))
         assert len(rawtiffs) == 40
 
+        for protein in proteins:
+            if protein in os.path.basename(folder):
+                break
+
         image = Image(
             "Confocal", 256, 256, 31, 3, 40, order="XYZCT", type="uint16")
-        image.add_channel("POI", -1)
-        image.add_channel("NUC", -1)
+        image.add_channel(protein, 16711935)
+        image.add_channel("DNA", 65535)
         image.add_channel("NEG_Dextran", -1)
+        image.data['Pixels']['PhysicalSizeX'] = '0.2516'
+        image.data['Pixels']['PhysicalSizeXUnit'] = 'µm'
+        image.data['Pixels']['PhysicalSizeY'] = '0.2516'
+        image.data['Pixels']['PhysicalSizeYUnit'] = 'µm'
+        image.data['Pixels']['PhysicalSizeZ'] = '0.75'
+        image.data['Pixels']['PhysicalSizeZUnit'] = 'µm'
         for i in range(40):
             image.add_tiff(
                 '%s/rawtif/%s' % (os.path.basename(cell), rawtiffs[i]),
@@ -48,8 +60,14 @@ for folder in folders:
         masktiffs = sorted(map(
             os.path.basename, glob.glob("%s/masktif/*" % cell)))
         image = Image("Mask", 256, 256, 31, 2, 40, order="XYZCT", type="int8")
-        image.add_channel("NUC", -1)
-        image.add_channel("CELL", -1)
+        image.add_channel("DNA", -1)
+        image.add_channel("Cell", -1)
+        image.data['Pixels']['PhysicalSizeX'] = '0.2516'
+        image.data['Pixels']['PhysicalSizeXUnit'] = 'µm'
+        image.data['Pixels']['PhysicalSizeY'] = '0.2516'
+        image.data['Pixels']['PhysicalSizeYUnit'] = 'µm'
+        image.data['Pixels']['PhysicalSizeZ'] = '0.75'
+        image.data['Pixels']['PhysicalSizeZUnit'] = 'µm'
         for i in range(40):
             image.add_tiff(
                 '%s/masktif/%s' % (os.path.basename(cell), masktiffs[i]),
@@ -61,7 +79,13 @@ for folder in folders:
         image = Image(
             "Concentration", 256, 256, 31, 1, 40, order="XYZCT",
             type="float")
-        image.add_channel("POI_conc", -1)
+        image.add_channel(protein, -1)
+        image.data['Pixels']['PhysicalSizeX'] = '0.2516'
+        image.data['Pixels']['PhysicalSizeXUnit'] = 'µm'
+        image.data['Pixels']['PhysicalSizeY'] = '0.2516'
+        image.data['Pixels']['PhysicalSizeYUnit'] = 'µm'
+        image.data['Pixels']['PhysicalSizeZ'] = '0.75'
+        image.data['Pixels']['PhysicalSizeZUnit'] = 'µm'
         for i in range(40):
             image.add_tiff(
                 '%s/conctif/%s' % (os.path.basename(cell), conctiffs[i]),
