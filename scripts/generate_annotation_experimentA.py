5e7b60c77c0076f910ae833446136555e3ad5013
Sebastien Besson <seb.besson@gmail.com>
2019-03-25 21:15:01 +0000

    Check that annotation are identical for the same image

--- scripts/generate_annotation_experimentA.py
+++ scripts/generate_annotation_experimentA.py
@@ -16,35 +16,46 @@ ANNOTATION_FILE = join(
 KEYS = [
     "Assay Name", "Dataset Name", "Image File", "Comment [Image File Path]"]
 
+
 with open(ASSAYS_FILE, 'r') as input_file:
     with open(ANNOTATION_FILE, 'w') as target_file:
+        # Read original assay file
         source_csv = csv.reader(input_file, delimiter='\t')
         source_headers = next(source_csv)
-        index = {key: source_headers.index(key) for key in KEYS}
+
+        # Create index
+        index = {key: source_headers.index(key) for key in source_headers}
 
         # Write CSV headers
         target_csv = csv.writer(target_file, delimiter='\t')
         target_headers = ['Dataset Name', 'Image Name'] + source_headers
         target_csv.writerow(target_headers)
+
+        last_row = []
         for source_row in source_csv:
             # Ignore mask rows
             image_file_path = source_row[index["Comment [Image File Path]"]]
             if 'masktif' in image_file_path:
                 continue
-            if not source_row[index["Image File"]].endswith("T0001.tif"):
-                continue
 
+            # Ensure all annotations are consistent for all timepoints
+            if not source_row[index["Image File"]].endswith("T0001.tif"):
+                for i in range(len(source_row)):
+                    if i != index["Image File"]:
+                        assert source_row[i] == last_row[i]
+                continue
+            last_row = source_row
+
+            # Generate imported dataset and image names from
             assay_name = source_row[index["Assay Name"]]
             protein = get_protein(assay_name)
             date = get_date(assay_name)
-
-            # Map dataset name and image name
             if 'conctif' in image_file_path:
                 dataset_name = "%s %s conc" % (protein, date)
             else:
                 dataset_name = "%s %s raw" % (protein, date)
             image_name = source_row[index["Dataset Name"]] + ".companion.ome"
 
-            # Write CSV row
+            # Write row in CSV file
             target_row = [dataset_name, image_name] + source_row
             target_csv.writerow(target_row)
